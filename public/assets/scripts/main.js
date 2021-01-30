55344b05f448d2a2560f620aec13363678e48d31
Erik <62840687+nausea87@users.noreply.github.com>
2021-01-30 15:50:56 +0100

    root fix

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -5,7 +5,7 @@ const messages = document.querySelectorAll(".messages");
 messages.forEach(message => {
     setTimeout(() => {
         message.parentElement.removeChild(message);
-    }, 1000);
+    }, 3000);
     console.log('this works!');
 });
 
